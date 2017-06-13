4f826a78a23bf0d75901cd2c9e2d8894cc19e89b
chen0907 <cfeng@gmu.edu>
2017-06-13 15:14:31 -0400

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+static int t = 1;
+
+int main()
+{
+    if (t)
+        printf("on this line\n");
+    else
+        printf("but not here\n");
+	t=0;
+	if (t)
+        printf("on this line\n");
+    else
+        printf("but not here\n");
+    return 0;
+}
